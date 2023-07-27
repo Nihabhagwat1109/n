d8817b17b5535cbb29b74fa3f87c3326b1f73331
Nihabhagwat1109 <136458091+Nihabhagwat1109@users.noreply.github.com>
2023-07-27 11:29:45 +0530

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -2,4 +2,4 @@ n1= int(input("enter marks 1:"))
 n2= int(input("enter marks 2:"))
 n3= int(input("enter marks 3:"))
 avg=(n1+n2+n3)/3
-print("average of n1, n2, n3 is:", avg)
+print("average of 10, 20, 30 is:", avg)
